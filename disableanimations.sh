41f2a844852f53bbf2e7bdc13806f2cf64c73c3e
ward5j <jon.ward@kbslp.com>
2016-08-12 07:28:40 -0700

    init

---
+++ disableanimations.sh
@@ -0,0 +1,39 @@
+# OS X user interface
+# Disable animations when opening and closing windows.
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+# Disable animations when opening a Quick Look window.
+defaults write -g QLPanelAnimationDuration -float 0
+
+# Accelerated playback when adjusting the window size (Cocoa applications). SET
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+# Disable animation when opening the Info window in OS X Finder (cmd⌘ + i). SET
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Disable animations when you open an application from the Dock. SET
+defaults write com.apple.dock launchanim -bool false
+
+# Make all animations faster that are used by Mission Control. SET
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Disable the delay when you hide the Dock
+defaults write com.apple.Dock autohide-delay -float 0
+
+# Mail applicatie
+# Disable the animation when you sending and replying an e-mail
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+
+# Safari
+# Disable the standard delay in rendering a Web page. SET
+defaults write com.apple.Safari WebKitInitialTimedLayoutDelay 0.25
+
+# The keyboard react faster to keystrokes (not equally useful for everyone)
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# If you want any of the above commands undo than is possible using the defaults delete command.
+# defaults delete domain key
+# Example: To undo the adjustment number 10, type the command:
+# defaults delete NSGlobalDomain KeyRepeat
+# After undoing, you need to restart your Mac.
